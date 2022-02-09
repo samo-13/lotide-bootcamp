e3b6381f6a7d6e4a60e270e604f0750991e7f8be
Sarah Moss <sarah-moss@live.com>
2022-02-09 15:19:56 +0000

    Adding first attempt at eqArrays to lotide project.

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+// SIMPLE FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+// Changed the names for actual and expected? Also tried console.log instead of return
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  } return true;
+  }
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+
+// TEST CODE for assertEqual
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 2);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1);
