a9c2c2aaf69ba37eaae13104468edfb2bb77be26
Sarah Moss <sarah-moss@live.com>
2022-02-11 03:11:03 +0000

    d03 lotide assignment

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${actual}`);
+  }
+};
+
+
+
+
+const findKeyByValue = function(object, value) {
+  for (const objectKeys in object) {
+    if (object[objectKeys] === value) {
+      return objectKeys;
+    }
+  }
+};
+
+// TEST CODE
+/* assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 2);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 1); */
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
